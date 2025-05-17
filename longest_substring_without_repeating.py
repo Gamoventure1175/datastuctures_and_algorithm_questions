01e4d5ada3c26451390f3ccfe04e4023e07b39ba
Gamoventure1175 <gauravmahajan1175@gmail.com>
2025-05-17 20:40:51 +0530

    Finally longest string is o(n)

--- longest_substring_without_repeating.py
+++ longest_substring_without_repeating.py
@@ -22,7 +22,7 @@
 
 # print(longest_substring('abcdefacbcbbbzywmijlgetuvqqabcdefghijklmnoprstuv'))
 
-#Trying a sliding window approach
+#Trying a sliding window approach 
 def longest_substring_sliding_window(sample: str) -> int:
     k = 1
     max_sub_str = 0
@@ -48,4 +48,38 @@ def longest_substring_sliding_window(sample: str) -> int:
             
     return max_sub_str
 
-print(longest_substring_sliding_window('abcdefacbcbbbzywmijlgetuvqqabcdefghijklmnoprstuv'))
+#The above approach is not exactly a sliding window approach because it recreates a window everytime with the following commands:
+#unique_set.clear()
+#unique_set.update(sample[l:r+1])
+
+# print(longest_substring_sliding_window('abcdefacbcbbbzywmijlgetuvqqabcdefghijklmnoprstuv'))
+
+
+
+#Sliding window approach
+def sliding_window_longest_string(sample: str) -> int:
+    k = 1
+    max_sub = 0
+    l = 0
+    r = 0
+    
+    unique_set = set()
+    
+    while r <= len(sample)-1:
+        if sample[r] not in unique_set:
+            if k > max_sub: 
+                max_sub = k
+            unique_set.add(sample[r])
+            r+=1
+            k+=1
+        else:
+            while sample[r] in unique_set:
+                unique_set.remove(sample[l])
+                l+=1
+            unique_set.add(sample[r])
+            r+=1
+            k=2
+            
+    return max_sub
+
+print(sliding_window_longest_string('abcdefacbcbbbzywmijlgetuvqqabcdefghijklmnoprstuv'))
