51473b0ce95c3eefd97afd05a742f68da5192af1
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2025-08-27 13:01:28 +0530

    Trying to solve the minimum stack problem

--- problems/minimum_stack.py
+++ problems/minimum_stack.py
@@ -13,48 +13,49 @@ You must implement a solution with O(1) time complexity for each function.
 
 
 # Approach 1: Python's list based stack
-class MinStack:
-    def __init__(self):
-        self._n = 0
-        self._stack = []
-        self._Min: int | None = None
+# class MinStack:
+#     def __init__(self):
+#         self._n = 0
+#         self._stack = []
+#         self._Min: int | None = None
 
-    def push(self, val: int):
-        self._Min = min(self._Min, val) if self._Min else val
+#     def push(self, val: int):
+#         self._Min = min(self._Min, val) if self._Min else val
 
-        self._stack.append(val)
-        self._n += 1
+#         self._stack.append(val)
+#         self._n += 1
 
-    def __len__(self):
-        return self._n
+#     def __len__(self):
+#         return self._n
 
-    def __repr__(self):
-        sample = ""
-        for i in range(self._n - 1, -1, -1):
-            sample += "| " + str(self._stack[i]) + " |" + "\n"
-        return sample
+#     def __repr__(self):
+#         sample = ""
+#         for i in range(self._n - 1, -1, -1):
+#             sample += "| " + str(self._stack[i]) + " |" + "\n"
+#         return sample
 
-    def pop(self):
-        if self._n == 0:
-            raise Exception("Cannot pop from an empty stack")
-        self._n -= 1
-        self._Min = None
-        popped_obj = self._stack.pop()
-        for val in self._stack:
-            self._Min = min(self._Min, val) if self._Min is not None else val
-        return popped_obj
+#     def pop(self):
+#         if self._n == 0:
+#             raise Exception("Cannot pop from an empty stack")
+#         self._n -= 1
+#         self._Min = None
+#         popped_obj = self._stack.pop()
+#         for val in self._stack:
+#             self._Min = min(self._Min, val) if self._Min is not None else val
+#         return popped_obj
 
-    def top(self):
-        if self._n == 0:
-            raise Exception("Cannot peek from an empty stack")
-        return self._stack[self._n - 1]
+#     def top(self):
+#         if self._n == 0:
+#             raise Exception("Cannot peek from an empty stack")
+#         return self._stack[self._n - 1]
 
-    def getMin(self):
-        if self._n == 0:
-            raise Exception("Cannot get minimum element from an empty stack")
+#     def getMin(self):
+#         if self._n == 0:
+#             raise Exception("Cannot get minimum element from an empty stack")
+#         return self._Min
 
-        return self._Min
 
+# Approach 2: 
 
 if __name__ == "__main__":
     # test = MinStack()
@@ -83,10 +84,10 @@ if __name__ == "__main__":
     # # Minimum element from the stack
     # print(f"Minimum element from the stack: {test.getMin()}")
 
-    some_test = MinStack()
-    some_test.push(0)
-    some_test.push(1)
-    some_test.push(0)
-    print(some_test.getMin())
-    some_test.pop()
-    print(some_test._Min)
+    # some_test = MinStack()
+    # some_test.push(0)
+    # some_test.push(1)
+    # some_test.push(0)
+    # print(some_test.getMin())
+    # some_test.pop()
+    # print(some_test._Min)
