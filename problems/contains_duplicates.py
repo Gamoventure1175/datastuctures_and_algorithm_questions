e0ae95ef7aa5f9856ae34d47882a6a68e9a35167
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2025-11-03 00:20:50 +0530

    Successfully solved this problem

---
+++ problems/contains_duplicates.py
@@ -0,0 +1,29 @@
+"""
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+"""
+
+# Approach 1 : Using sets and early exit strategy
+
+
+def hasDuplicate(nums):
+
+    existing_nums = set()
+
+    for num in nums:
+        if num in existing_nums:
+            return True
+        existing_nums.add(num)
+
+    return False
