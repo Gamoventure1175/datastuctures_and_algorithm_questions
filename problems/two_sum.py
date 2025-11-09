f1cc201c4ab46b02062775253f3b4d76f4d2c6dd
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2025-11-10 01:35:44 +0530

    Resolved two_sum problem
    It works but still needs improvement

--- problems/two_sum.py
+++ problems/two_sum.py
@@ -1,4 +1,4 @@
-'''
+"""
 Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
 
 You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
@@ -7,7 +7,7 @@ Return the answer with the smaller index first.
 
 Example 1:
 
-Input: 
+Input:
 nums = [3,4,5,6], target = 7
 
 Output: [0,1]
@@ -28,34 +28,67 @@ Constraints:
 2 <= nums.length <= 1000
 -10,000,000 <= nums[i] <= 10,000,000
 -10,000,000 <= target <= 10,000,000
-'''
+"""
 
 # I have already solved this problem using brute force method. However, it was o(n**2).
 
 # Approach 2: Using dictionary to keep track of all the elements in the given array
 
-def twoSum(nums: list[int], target: int):
-    nums_dict: dict[int, int] = {}
-    
-    for i in range(len(nums)):
-        nums_dict[nums[i]] = i
-    
-    for i in range(len(nums)):
-        diff = target - nums[i]
-        
-        if nums_dict.get(diff):
-            found = nums_dict[diff]
-            
-            if found == i:
-                continue
-            
-            return [i, found] if i < found else [found, i]
+# def twoSum(nums: list[int], target: int):
+#     nums_dict: dict[int, int] = {}
 
-    return False
+#     for i in range(len(nums)):
+#         nums_dict[nums[i]] = i
+
+#     for i in range(len(nums)):
+#         diff = target - nums[i]
+
+#         if nums_dict.get(diff):
+#             found = nums_dict[diff]
+
+#             if found == i:
+#                 continue
+
+#             return [i, found] if i < found else [found, i]
+
+#     return False
+
+
+# if __name__ == "__main__":
+
+#     nums = [1,3,4,2,]
+#     target = 6
+
+#     print(twoSum(nums, target))
+
+
+# Approach 3:
+def twoSum(nums: list[int], target: int):
+    """
+    Finds the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+    Params:
+        - nums: list of numbers
+        - target: value to find indices for
+
+    Return:
+        - list[int]: pair of indices i and j
+
+    Note: Assumption that every input has exactly one pair of
+    indices i and j that satisfy the condition.
+    """
+
+    indices_tracker = {nums[i]: i for i in range(len(nums))}
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        possible_index = indices_tracker.get(complement, None)
+
+        if possible_index != None and i != possible_index:
+            return [i, possible_index]
+
 
 if __name__ == "__main__":
-    
-    nums = [1,3,4,2,]
+    nums = [1, 3, 4, 2]
     target = 6
-    
     print(twoSum(nums, target))
