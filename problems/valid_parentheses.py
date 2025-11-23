9f1bc5bc636a6762498b36673a106ec80bb63ee2
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2025-11-24 00:54:36 +0530

    Solved the valid parentheses problem

---
+++ problems/valid_parentheses.py
@@ -0,0 +1,64 @@
+"""
+You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Return true if s is a valid string, and false otherwise.
+
+Example 1:
+
+Input: s = "[]"
+
+Output: true
+Example 2:
+
+Input: s = "([{}])"
+
+Output: true
+Example 3:
+
+Input: s = "[(])"
+
+Output: false
+Explanation: The brackets are not closed in the correct order.
+
+Constraints:
+
+1 <= s.length <= 1000
+
+"""
+
+# Approach 1 : Using a stack
+
+
+def valid_parentheses(s: str):
+    if len(s) // 2 == 1:
+        return False
+
+    closing_brackets = {"[": "]", "(": ")", "{": "}"}
+
+    brackets_stack = []
+
+    for char in s:
+        if char in "({[":
+            brackets_stack.append(closing_brackets[char])
+        elif char in "})]":
+            if not brackets_stack:
+                return False
+
+            if char != brackets_stack.pop():
+                return False
+
+    if not brackets_stack:
+        return False
+
+    return True
+
+
+if __name__ == "__main__":
+    input = "}])"
+
+    print(valid_parentheses(input))
