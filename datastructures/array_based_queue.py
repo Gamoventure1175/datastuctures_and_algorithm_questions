c7d5acc3da4c36ad14ead77162c78d5e3ffba3ec
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2026-02-28 10:31:47 +0530

    Understood the working of circular array based queue implemenetation

--- datastructures/array_based_queue.py
+++ datastructures/array_based_queue.py
@@ -1,98 +1,109 @@
 # The following operations must be the ones that a Queue must have
-# Queue.enqueue() -> add element to start of the queue
-# Queue.dequeue() -> remove element from the end of the queue 
+# Queue.enqueue() -> add element to end of the queue
+# Queue.dequeue() -> remove element from the start of the queue
 #                    and raise an exception if the queue is empty
 # Queue.first() -> return the first element from the queue
 # Q.is_empty() -> return a True if the queue is empty or else False
 # len(Q) -> return the number of elements in the queue
 
 
-# Making a python list based stack
+# Making a python circular array based queue
+
 
 class Empty(Exception):
-    '''Raise an empty exception if the queue is empty'''
+    """Raise an empty exception if the queue is empty"""
 
-class ArrayQueue():
-    '''FIFO queue implementation using a python list as an underlying storage'''
+
+class CircularArrayQueue:
+    """FIFO queue implementation using a python list as an underlying storage"""
+
     DEFAULT_CAPACITY = 10
 
     def __init__(self):
-        '''Create an empty Queue'''
-        self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
-        self._size = 0 #actual elements
+        """Create an empty Queue"""
+        self._data = [None] * CircularArrayQueue.DEFAULT_CAPACITY
+        self._size = 0  # actual elements
         self._front = 0
-    
+
     def enqueue(self, e):
-        '''Adds an element to the back of the queue'''
+        """Adds an element to the back of the queue"""
         if self._size == len(self._data):
             self._resize(2 * len(self._data))
         avail = (self._front + self._size) % len(self._data)
         self._data[avail] = e
         self._size += 1
-        
+
     def dequeue(self):
-        '''Removes an element from the end of the queue
-        
-        Raises Empty exception if teh queue is empty'''
+        """Removes an element from the front of the queue
+
+        Raises Empty exception if teh queue is empty"""
         if self.is_empty():
             raise Empty("Cannot dequeue from an empty queue")
-        
+
         answer = self._data[self._front]
         self._data[self._front] = None
         self._front = (self._front + 1) % len(self._data)
         self._size -= 1
-        
-        if 0 < self._size < len(self._data) // 4:
+
+        if (
+            0 < self._size < len(self._data) // 4
+            and len(self._data) > CircularArrayQueue.DEFAULT_CAPACITY
+        ):
             self._resize(len(self._data) // 2)
         return answer
-    
+
     def _resize(self, cap):
-        '''Resize the queue by cap * previous size'''
+        """Resize the queue by cap * previous size"""
         old = self._data
         self._data = [None] * cap
         walk = self._front
-        for k in range(len(old)):
+        for k in range(self._size):
             self._data[k] = old[walk]
-            walk = (1+walk) % len(old)
+            walk = (1 + walk) % len(old)
         self._front = 0
-    
+
     def first(self):
-        '''Return a reference of the first element in the queue'''
+        """Return a reference of the first element in the queue"""
         if self.is_empty():
             raise Empty("Queue is empty")
         return self._data[self._front]
 
     def is_empty(self):
-        '''Return a boolean value based on if the queue is empty or not'''
+        """Return a boolean value based on if the queue is empty or not"""
         return self._size == 0
-    
+
     def __len__(self):
-        '''Return the actual number of elements in the queue'''
+        """Return the actual number of elements in the queue"""
         return self._size
-        
+
     def __repr__(self):
         """A better representation of the queue"""
-        format = "in -> [ "
+        format = "out <- [ "
         for j in range(len(self._data)):
             format += str(self._data[j]) + ", "
-        format += "] -> out"
+        format += "] <- in"
         return format
 
 
 if __name__ == "__main__":
-    example = ArrayQueue()
-    for number in range(1, 16):
+    example = CircularArrayQueue()
+    for number in range(1, 10):
         example.enqueue(number)
-        print(f'Current queue: {example}. Current size of the queue: {example._size}. Current front: {example._front} \n')
-        
-    for numbers in range(1, 5):
+        print(
+            f"Current queue: {example}. Current size of the queue: {len(example)}. Current front: {example.first()} \n"
+        )
+
+    for numbers in range(1, 3):
         example.dequeue()
-    
+
     print(f"Current queue after some deletions: {example}")
-    
-    for number in range(16, 28):
+    print(f"Current first: {example.first()}")
+
+    for number in range(10, 16):
         example.enqueue(number)
-        print(f'Current queue: {example}. Current size of the queue: {example._size}. Current front: {example._front} \n')
+        print(
+            f"Current queue: {example}. Current size of the queue: {len(example)}. Current front: {example.first()} \n"
+        )
 
 
 # [1,2,3,4,5] <- in -> append
@@ -103,4 +114,4 @@ if __name__ == "__main__":
 # [1,.....100] -> enque
 # [] deque -> 50 elements -> [] -> shrink -> none
 
-#length = n, actual elements after deque n/4 -> [] / 2
+# length = n, actual elements after deque n/4 -> [] / 2
