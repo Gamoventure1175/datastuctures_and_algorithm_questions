8d20f972f298fbf8f375900f0b0c20028ddb020d
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2026-03-08 01:24:04 +0530

    Finally solved the permutation in string problem using sliding window and understood the concept

--- problems/permutaion_in_string.py
+++ problems/permutaion_in_string.py
@@ -22,11 +22,34 @@ Constraints:
 1 <= s1.length, s2.length <= 1000
 """
 
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         k = len(s1)
-        s1_char_map = {char : 0 for char in s1}
-
+        s1_freq_map = {}
+        for char in s1:
+            s1_freq_map[char] = s1_freq_map.get(char, 0) + 1
+        # print("S1 map: ", s1_freq_map)
         l = 0
+        current_map = {}
         for r in range(len(s2)):
-            
+            current_map[s2[r]] = current_map.get(s2[r], 0) + 1
+
+            if (r - l + 1) > k:
+                current_map[s2[l]] = current_map[s2[l]] - 1
+                if current_map[s2[l]] < 1:
+                    current_map.pop(s2[l])
+                l += 1
+            # print("Current map: ", current_map, "S1 map: ", s1_freq_map)
+
+            if current_map == s1_freq_map:
+                return True
+
+        return False
+
+
+if __name__ == "__main__":
+    question = Solution()
+    s1 = "abc"
+    s2 = "lecaabee"
+    print(question.checkInclusion(s1, s2))
