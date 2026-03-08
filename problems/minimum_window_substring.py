dc043a6410d5c930ecad3edbc3d818d64d1a1de6
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2026-03-08 11:52:17 +0530

    Failed solving the minimum-window-substring problem

---
+++ problems/minimum_window_substring.py
@@ -0,0 +1,78 @@
+"""
+Given two strings s and t, return the shortest substring of s such that every character in t, including duplicates, is present in the substring. If such a substring does not exist, return an empty string "".
+
+You may assume that the correct output is always unique.
+
+Example 1:
+
+Input: s = "OUZODYXAZV", t = "XYZ"
+
+Output: "YXAZ"
+Explanation: "YXAZ" is the shortest substring that includes "X", "Y", and "Z" from string t.
+
+Example 2:
+
+Input: s = "xyz", t = "xyz"
+
+Output: "xyz"
+Example 3:
+
+Input: s = "x", t = "xy"
+
+Output: ""
+Constraints:
+
+1 <= s.length <= 1000
+1 <= t.length <= 1000
+s and t consist of uppercase and lowercase English letters.
+"""
+
+
+# Appraoch 1: Failed
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        t_char_map = {}
+        for char in t:
+            t_char_map[char] = t_char_map.get(char, 0) + 1
+
+        need = len(t_char_map)
+        have = 0
+
+        current_window_map = {}
+        minl = len(s)
+        shortest_substring = ""
+
+        l = r = 0
+        while r < len(s):
+            if s[r] in t_char_map and (
+                t_char_map[s[r]] != current_window_map.get(s[r], 0)
+            ):
+                current_window_map[s[r]] = current_window_map.get(s[r], 0) + 1
+                have += 1
+
+            print(current_window_map)
+
+            while have == need:
+                if (r - l + 1) < minl:
+                    minl = r - l + 1
+                    shortest_substring = s[l : r + 1]
+
+                current_window_map[s[l]] -= 1
+                if current_window_map.get(s[l], 0) < 1:
+                    current_window_map.pop(s[l])
+                if s[l] in t_char_map and (
+                    t_char_map[s[l]] == current_window_map.get(s[l], 0)
+                ):
+                    have -= 1
+                l += 1
+            r += 1
+
+        return shortest_substring
+
+
+if __name__ == "__main__":
+    question = Solution()
+    s = "aaaaaaaaaaaabbbbbcdd"
+    t = "abcdd"
+
+    print(question.minWindow(s, t))
