e5636d6967cdfda6235ecdd793239f6fec16b53b
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2026-03-09 19:37:05 +0530

    Finally solved the minim window substring problem

--- problems/minimum_window_substring.py
+++ problems/minimum_window_substring.py
@@ -27,52 +27,90 @@ Constraints:
 s and t consist of uppercase and lowercase English letters.
 """
 
-
 # Appraoch 1: Failed
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         t_char_map = {}
+#         for char in t:
+#             t_char_map[char] = t_char_map.get(char, 0) + 1
+
+#         need = len(t_char_map)
+#         have = 0
+
+#         current_window_map = {}
+#         minl = len(s)
+#         shortest_substring = ""
+
+#         l = r = 0
+#         while r < len(s):
+#             if s[r] in t_char_map and (
+#                 t_char_map[s[r]] != current_window_map.get(s[r], 0)
+#             ):
+#                 current_window_map[s[r]] = current_window_map.get(s[r], 0) + 1
+#                 have += 1
+
+#             print(current_window_map)
+
+#             while have == need:
+#                 if (r - l + 1) < minl:
+#                     minl = r - l + 1
+#                     shortest_substring = s[l : r + 1]
+
+#                 current_window_map[s[l]] -= 1
+#                 if current_window_map.get(s[l], 0) < 1:
+#                     current_window_map.pop(s[l])
+#                 if s[l] in t_char_map and (
+#                     t_char_map[s[l]] == current_window_map.get(s[l], 0)
+#                 ):
+#                     have -= 1
+#                 l += 1
+#             r += 1
+
+#         return shortest_substring
+
+
+# if __name__ == "__main__":
+#     question = Solution()
+#     s = "aaaaaaaaaaaabbbbbcdd"
+#     t = "abcdd"
+
+#     print(question.minWindow(s, t))
+
+
+# Approach 2: using
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        t_char_map = {}
+        countT, window = {}, {}
+
+        if t == "":
+            return ""
+
         for char in t:
-            t_char_map[char] = t_char_map.get(char, 0) + 1
+            countT[char] = countT.get(char, 0) + 1
 
-        need = len(t_char_map)
-        have = 0
+        have, need = 0, len(countT)
 
-        current_window_map = {}
-        minl = len(s)
-        shortest_substring = ""
+        result, min_l = [-1, -1], float("infinity")
 
-        l = r = 0
-        while r < len(s):
-            if s[r] in t_char_map and (
-                t_char_map[s[r]] != current_window_map.get(s[r], 0)
-            ):
-                current_window_map[s[r]] = current_window_map.get(s[r], 0) + 1
-                have += 1
+        l = 0
+        for r in range(len(s)):
+            if s[r] in countT:
+                window[s[r]] = window.get(s[r], 0) + 1
 
-            print(current_window_map)
+                if window[s[r]] == countT[s[r]]:
+                    have += 1
 
             while have == need:
-                if (r - l + 1) < minl:
-                    minl = r - l + 1
-                    shortest_substring = s[l : r + 1]
+                if (r - l + 1) < min_l:
+                    min_l = r - l + 1
+                    result = [l, r]
 
-                current_window_map[s[l]] -= 1
-                if current_window_map.get(s[l], 0) < 1:
-                    current_window_map.pop(s[l])
-                if s[l] in t_char_map and (
-                    t_char_map[s[l]] == current_window_map.get(s[l], 0)
-                ):
-                    have -= 1
+                if s[l] in countT:
+                    window[s[l]] -= 1
+
+                    if window[s[l]] < countT[s[l]]:
+                        have -= 1
+
                 l += 1
-            r += 1
 
-        return shortest_substring
-
-
-if __name__ == "__main__":
-    question = Solution()
-    s = "aaaaaaaaaaaabbbbbcdd"
-    t = "abcdd"
-
-    print(question.minWindow(s, t))
+        return s[result[0] : result[1] + 1] if min_l != float("infinity") else ""
