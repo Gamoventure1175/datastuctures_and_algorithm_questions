3885f91f62137cf837b1e3851bc76aebf67dd664
Gaurav Abhiman Mahajan <109190973+Gamoventure1175@users.noreply.github.com>
2026-06-16 14:35:26 +0530

    Solved reverse linked list problem

---
+++ linked_list/reverse_linked_list.py
@@ -0,0 +1,48 @@
+"""
+
+Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+Example 1:
+
+Input: head = [0,1,2,3]
+
+Output: [3,2,1,0]
+Example 2:
+
+Input: head = []
+
+Output: []
+Constraints:
+
+0 <= The length of the list <= 1000.
+-1000 <= Node.val <= 1000
+"""
+
+# Using pointers to reverse the list in place
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        if curr is None:
+            return
+
+        while curr is not None:
+            temp = curr.next
+            curr.next = prev
+
+            prev = curr
+            curr = temp
+
+        return prev
