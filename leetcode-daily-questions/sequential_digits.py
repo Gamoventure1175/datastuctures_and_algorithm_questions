4ca074b26675970e3b13b1962dbae8cb4b35fc19
Gaurav Abhiman Mahajan <gauravmahajan1175@gmail.com>
2026-07-13 11:35:12 +0530

    Failed to solve sequential digits problem

---
+++ leetcode-daily-questions/sequential_digits.py
@@ -0,0 +1,54 @@
+"""
+An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
+
+Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
+
+ 
+
+Example 1:
+
+Input: low = 100, high = 300
+Output: [123,234]
+Example 2:
+
+Input: low = 1000, high = 13000
+Output: [1234,2345,3456,4567,5678,6789,12345]
+ 
+
+Constraints:
+
+10 <= low <= high <= 10^9
+"""
+# Attempt 1: Using intuition
+
+# class Solution:
+#     def isSequential(self, n: int) -> bool:
+#         last_digit = n % 10
+#         n //= 10
+#         while n:
+#             curr = n % 10
+#             n //= 10
+
+#             if last_digit != (curr + 1):
+#                 return False
+            
+#             last_digit = curr
+
+#         return True
+
+#     def sequentialDigits(self, low: int, high: int) -> List[int]:
+#         result = []
+
+#         for i in range(low, high+1):
+#             if self.isSequential(i):
+#                 result.append(i)
+        
+#         return result
+
+# Attempt 2: Trying to find a better approach
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        
+
+if __name__ == "__main__":
+    sol = Solution()
