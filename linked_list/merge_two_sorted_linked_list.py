bcdc00379f0a99879b04e5fe6729c5f13c1b36ad
Gaurav Abhiman Mahajan <gauravmahajan1175@gmail.com>
2026-07-18 10:51:49 +0530

    solved: merge two sorted linked list

--- linked_list/merge_two_sorted_linked_list.py
+++ linked_list/merge_two_sorted_linked_list.py
@@ -38,38 +38,71 @@ class ListNode:
         self.next = next
 
 
+#class Solution:
+#    def mergeTwoLists(
+#        self, list1: Optional[ListNode], list2: Optional[ListNode]
+#    ) -> Optional[ListNode]:
+#        if not list1:
+#            return list2
+#
+#        if not list2:
+#            return list1
+#
+#        dummy = ListNode()
+#        temp = dummy
+#
+#        while list1 and list2:
+#            if list1.val <= list2.val:
+#                dummy.next = list1
+#                list1 = list1.next
+#                dummy = dummy.next
+#
+#            else:
+#                dummy.next = list2
+#                list2 = list2.next
+#                dummy = dummy.next
+#
+#        while list1:
+#            dummy.next = list1
+#            list1 = list1.next
+#            dummy = dummy.next
+#
+#        while list2:
+#            dummy.next = list2
+#            list2 = list2.next
+#            dummy = dummy.next
+#
+#        return temp.next
+
+# Attempt 2: Using recursion
 class Solution:
-    def mergeTwoLists(
-        self, list1: Optional[ListNode], list2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        if not list1:
-            return list2
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        temp = head
+        def traversal(list1, list2):
+            nonlocal head
 
-        if not list2:
-            return list1
+            if list1 and list2:
+                if list1.val <= list2.val:
+                    head.next = list1
+                    head = head.next
+                    return traversal(list1.next, list2)
 
-        dummy = ListNode()
-        temp = dummy
+                head.next = list2
+                head = head.next
+                return traversal(list1, list2.next)
+            
+            elif list1 is None and list2:
+                head.next = list2
+                head = head.next
+                return traversal(list1, list2.next)
+            
+            elif list2 is None and list1:
+                head.next = list1
+                head = head.next
+                return traversal(list1.next, list2)
+            
+            return
 
-        while list1 and list2:
-            if list1.val <= list2.val:
-                dummy.next = list1
-                list1 = list1.next
-                dummy = dummy.next
-
-            else:
-                dummy.next = list2
-                list2 = list2.next
-                dummy = dummy.next
-
-        while list1:
-            dummy.next = list1
-            list1 = list1.next
-            dummy = dummy.next
-
-        while list2:
-            dummy.next = list2
-            list2 = list2.next
-            dummy = dummy.next
-
+        traversal(list1, list2)
         return temp.next
